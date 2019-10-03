a5937814e486aee9281ef380d630eb2a62be22ec
Wojciech Golab <spioooszek@gmail.com>
2019-10-03 19:42:41 +0200

    Merge of recurrence pattern to event

--- Src/Calendar.DAL/Events/EventEntity.cs
+++ Src/Calendar.DAL/Events/EventEntity.cs
@@ -1,4 +1,5 @@
 using System;
+using Calendar.Common.Events;
 
 namespace Calendar.DAL.Events
 {
@@ -10,6 +11,10 @@ namespace Calendar.DAL.Events
         public DateTime EndDate { get; set; }
         public int Duration { get; set; }
         public bool IsAllDay { get; set; }
-        public RecurrencePatternEntity RecurrencePattern { get; set; }
+        public FrequencyType FrequencyType { get; set; }
+        public RecurrenceType RecurrenceType { get; set; }
+        public int Interval { get; set; }
+        public int? Occurences { get; set; }
+        public Days Days { get; set; } 
     }
 }
