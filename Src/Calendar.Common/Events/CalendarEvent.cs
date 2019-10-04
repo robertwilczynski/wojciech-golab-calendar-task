0f652f25bbf21d0bd53b7c8a3c2215ca4a773a60
Wojciech Golab <spioooszek@gmail.com>
2019-10-04 12:01:25 +0200

    Recurrence pattern model removed

--- Src/Calendar.Common/Events/CalendarEvent.cs
+++ Src/Calendar.Common/Events/CalendarEvent.cs
@@ -11,8 +11,12 @@ namespace Calendar.Common.Events
         public DateTime EndDate { get; set; }
         public int Duration { get; set; }
         public bool IsAllDay { get; set; }
-        public bool IsRecurring => RecurrencePattern != null;
+        public bool IsRecurring => RecurrenceType != RecurrenceType.None;
         public List<DateTime> ExceptionDays { get; set; }
-        public RecurrencePattern RecurrencePattern { get; set; }
+        public FrequencyType FrequencyType { get; set; }
+        public RecurrenceType RecurrenceType { get; set; }
+        public int Interval { get; set; }
+        public int? Occurences { get; set; }
+        public List<DayOfWeek> Days { get; set; } 
     }
 }
