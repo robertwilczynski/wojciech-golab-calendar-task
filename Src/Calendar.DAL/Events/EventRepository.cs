0f21b2f5a7937bb125b6e47856d078b2d0881943
Wojciech Golab <spioooszek@gmail.com>
2019-10-07 09:59:34 +0200

    Added DayToWeek to Days convertion

--- Src/Calendar.DAL/Events/EventRepository.cs
+++ Src/Calendar.DAL/Events/EventRepository.cs
@@ -12,9 +12,52 @@ namespace Calendar.DAL.Events
             _dbContext = dbContext;
         }
         
+        private Days? TranslateToFlags(List<DayOfWeek> days)
+        {
+            Days? flags = null;
+            foreach (var day in days)
+            {
+                if(Enum.TryParse(day.ToString(), out Days result))
+                {
+                    if (flags == null)
+                    {
+                        flags = result;
+                    }
+                    else
+                    {
+                        flags = flags | result;
+                    }
+                }
+                else
+                {
+                    throw new FormatException($"Given day {day.ToString()} cannot be converted to Days enum");
+                }
+            }
+            return flags;
+        }
+
         public int AddEvent(CalendarEvent calendarEvent)
         {
-            throw new System.NotImplementedException();
+            if (calendarEvent == null)
+            {
+                throw new ArgumentNullException(nameof(calendarEvent));
+            }
+            var eventEntity = new EventEntity
+            {
+                Duration = calendarEvent.Duration,
+                EndDate = calendarEvent.EndDate,
+                FrequencyType = calendarEvent.FrequencyType,
+                Interval = calendarEvent.Interval,
+                IsAllDay = calendarEvent.IsAllDay,
+                Name = calendarEvent.Name,
+                Occurences = calendarEvent.Occurences,
+                RecurrenceType = calendarEvent.RecurrenceType,
+                StartDate = calendarEvent.StartDate,
+                Days = TranslateToFlags(calendarEvent.Days),
+            };
+            _dbContext.Events.Add(eventEntity);
+            _dbContext.SaveChanges();
+            return eventEntity.Id;
         }
 
         public int EditEvent(int calendarEventId, CalendarEvent calendarEvent)
