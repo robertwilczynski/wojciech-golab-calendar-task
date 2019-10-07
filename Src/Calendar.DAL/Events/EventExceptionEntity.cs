6e9b5b7ec9b69cfc4a568b4b0fda8faa0235aa7d
Wojciech Golab <spioooszek@gmail.com>
2019-10-07 16:39:52 +0200

    Changed naming of event entity in exceptions table

--- Src/Calendar.DAL/Events/EventExceptionEntity.cs
+++ Src/Calendar.DAL/Events/EventExceptionEntity.cs
@@ -5,7 +5,7 @@ namespace Calendar.DAL.Events
     public class EventExceptionEntity
     {
         public int Id { get; set; }
-        public EventEntity EventId { get; set; }
+        public EventEntity Event { get; set; }
         public DateTime ExcludedDate { get; set; }
     }
 }
